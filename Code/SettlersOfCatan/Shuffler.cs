69443a205faeb48349b9e80594c743130f53227c
Sam Kim <kimsj@rose-hulman.edu>
2012-04-18 22:41:04 -0400

    Add Shuffler class

---
+++ Code/SettlersOfCatan/Shuffler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SettlersOfCatan
+{
+    class Shuffler
+    {
+        public static void Shuffle(ArrayList source)
+        {
+            Random rnd = new Random();
+            for (int inx = source.Count - 1; inx > 0; --inx)
+            {
+                int position = rnd.Next(inx);
+                object temp = source[inx];
+                source[inx] = source[position];
+                source[position] = temp;
+            }
+
+        }
+    }
+}
